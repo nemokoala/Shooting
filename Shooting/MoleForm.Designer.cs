333f7290fcfd11bbc7f43ae92a59b1a4191911b8
DevLiuf <84566022+DevLiuf@users.noreply.github.com>
2022-11-26 18:04:38 +0900

    폼 크기 조정 불가 수정

--- Shooting/MoleForm.Designer.cs
+++ Shooting/MoleForm.Designer.cs
@@ -208,6 +208,7 @@
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.KeyPreview = true;
             this.Name = "MoleForm";
